8c5ba40832966e7444783fdfc639a63f4b9208ad
Alejandra Hernandez <128438204+DianaHernandez17@users.noreply.github.com>
2023-11-28 22:00:34 -0500

    Update index-2-component.js

--- src/index-2-component.js
+++ src/index-2-component.js
@@ -1,199 +1,293 @@
 import { LitElement, html, css, property } from "lit-element";
 import stylesScss from './index-componentStyles';
-import { renderPage } from './main.js';
+import { UserService } from "./main.js";
 
 
-export const usersData = [
-    {
-      
-      nombre: "Julian",
-      empresa: "Claro",
-      equipo: "Campaña A",
-      inicia: "Lunes",
-      termina: "Jueves",
-    },
-    {
-     
-        nombre: "Juli",
-        empresa: "movistar",
-        equipo: "Campaña A",
-        inicia: "Martes",
-        termina: "Sabado",
-    },
-    // Agrega más objetos con datos de usuarios y sus respectivas campañas
-  ];
+
 
 
 export class Index2 extends LitElement {
+  static get properties() {
+    return {
+      campnas: { type: Array },
+      campanasencontrado: { type: Object },
+    };
+  }
+
   constructor() {
     super();
+    this.campanas = UserService.campanas;
+    this.campanasencontrado = null;
+    this.tableData = JSON.parse(localStorage.getItem('tableData')) || [];
+  }
+
+  usuarios() {
+    Router.go("/usuarios");
+  }
+
+  campanas() {
+    Router.go("/campanas");
+  }
+
+  equipos() {
+    Router.go("/equipos");
+  }
+
+  showEquipoDetails(equip) {
+    this.campanasencontrado = equip;
+  }
+
+  closeEquipoDetails() {
+    this.campanasencontrado = null;
   }
 
   static get styles() {
-    return [stylesScss];
+    return [stylesScss, css`
+      /* Agregamos estilos específicos para mejorar la apariencia del formulario */
+      .modal-content {
+        max-width: 300px;
+        margin: auto;
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      }
+
+      input {
+        width: 100%;
+        padding: 8px;
+        margin: 8px 0;
+        box-sizing: border-box;
+      }
+
+      button {
+        background-color: #343A40;
+        color: white;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+
+      button:hover {
+        background-color: #555;
+      }
+    `];
   }
 
-  buscarUsuarios() {
-    if (this.nombre) {
-      const nombre = this.nombre.toLowerCase();
-  
-      // Filtra la lista de usuarios en función del nombre ingresado
-      const usuariosFiltrados = usersData.filter((user) => {
-        return user.nombre.toLowerCase().includes(nombre);
-      });
-  
-      return usuariosFiltrados;
-    }
-    // Devuelve una lista vacía si this.nombre no está definido
-    return [];
+  openModal() {
+    const modal = this.shadowRoot.getElementById('myModal');
+    modal.style.display = 'block';
   }
-  
 
-  openNewUserForm() {
-    const nombre = prompt("Ingrese el Nombre del usuario:");
-    const empresa = prompt("Ingrese la empresa:");
-    const equipo = prompt("Ingrese el equipo que se encuentra:");
-    const inicia = prompt("Ingrese cuando quiere que inicie:");
-    const termina = prompt("Ingrese cuando quiere que termine:");
+  closeModal() {
+    const modal = this.shadowRoot.getElementById('myModal');
+    modal.style.display = 'none';
+  }
 
-    if (nombre && empresa && equipo && inicia && termina) {
-      // Asegúrate de que los datos no sean nulos y luego agrega los datos a usersData.
-      usersData.push({ nombre, empresa, equipo, inicia, termina });
-      // Puedes hacer algo adicional con los datos si es necesario.
+  registrarCompania() {
+    const nombreElement = this.shadowRoot.getElementById('nombre');
+    const equipoElement = this.shadowRoot.getElementById('equipo');
+    const campanaElement = this.shadowRoot.getElementById('campana');
 
-      // Vuelve a renderizar para mostrar los nuevos datos en la lista.
+    if (nombreElement && equipoElement && campanaElement) {
+      const nombre = nombreElement.value;
+      const equipo = equipoElement.value;
+      const campana = campanaElement.value;
+
+      this.tableData.push({ nombre, equipo, campana, activa: true });
+      this.saveData();
       this.requestUpdate();
+      this.closeModal();
+    } else {
+      console.error('Los elementos no se han encontrado en el DOM.');
     }
   }
+
+  toggleCampaignStatus(index) {
+    this.tableData[index].activa = !this.tableData[index].activa;
+    this.saveData();
+    this.requestUpdate();
+  }
+
+  saveData() {
+    localStorage.setItem('tableData', JSON.stringify(this.tableData));
+  }
+
+ 
 
 
   render() {
     return html`
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <div class="d-flex ">
-    <div class="pt-2 d-flex justify-content-left ">
-        <div class="d-flex  flex-shrink-0 p-3 ml-5" style="width: 250px; background-color: rgb(201, 205, 207); border-radius: 1rem; height: 42.4rem;">
-            <hr>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
+
+      <div class="d-flex">
+        <div class="pt-2 d-flex justify-content-left">
+          <div
+            class="d-flex flex-shrink-0 p-3 ml-5"
+            style="width: 250px; background-color: rgb(201, 205, 207); border-radius: 1rem; height: 36rem;"
+          >
+            <hr />
             <ul class="nav nav-pills flex-column mb-auto" style="width: 25rem;">
-                <li class="nav-item">
-                    <button  @click="${() => renderPage('index')}" class="nav-link active bg-light pt-2 font-weight-bold" style="color: grey; border-radius: 10px; height: 45px; width: 13.5rem; font-size: 18px;" aria-current="page">
-                        <i class="fas fa-user me-2"></i> Usuarios
-                    </button>
-                </li>
-                <li class="nav-item pt-3">
-                    <button  @click="${() => renderPage('index2')}" @click="${() => renderPage('index')}" class="nav-link active bg-light pt-2 font-weight-bold" style="color: grey; border-radius: 10px; height: 45px; width: 13.5rem; font-size: 18px;" aria-current="page">
-                        <i class="fas fa-user me-2"></i> Campañas
-                    </button>
-                </li>
-                <li class="nav-item pt-3">
-                    <button class="nav-link active bg-light pt-2 font-weight-bold" style="color: grey; border-radius: 10px; height: 45px; width: 13.5rem; font-size: 18px;" aria-current="page">
-                        <i class="fas fa-user me-2"></i> Equipos
-                    </button>
-                </li>
+              <li class="nav-item">
+                <a
+                  href="#"href="#" @click="${() => renderPage('index')}"
+                  class="nav-link active bg-light pt-2 font-weight-bold"
+                  style="color: grey; border-radius: 10px; height: 45px; font-size: 18px;"
+                  aria-current="page"
+                >
+                  <i class="fas fa-user me-2"></i> Usuarios
+                </a>
+              </li>
+              <li class="nav-item pt-3">
+                <a
+                  href="#"
+                  @click="${() => renderPage('index2')}"
+                  class="nav-link active bg-light pt-2 font-weight-bold"
+                  style="color: grey; border-radius: 10px; height: 45px; font-size: 18px;"
+                  aria-current="page"
+                >
+                  <i class="fas fa-building me-2"></i>Campañas
+                </a>
+              </li>
+             <li class="nav-item pt-3">
+                <a
+                  href="#"
+                  @click="${() => renderPage('index3')}"
+                  class="nav-link active bg-light pt-2 font-weight-bold"
+                  style="color: grey; border-radius: 10px; height: 45px; font-size: 18px;"
+                  aria-current="page"
+                >
+                    <i class="fas fa-users me-2"></i>   Equipos
+                </a>
+              </li>
             </ul>
-            <hr>
+            <hr />
+          </div>
         </div>
-    </div>
-    &nbsp&nbsp&nbsp
-    &nbsp&nbsp&nbsp
-    <!-- cajas de texto-->
-    <div class="pt-2  align-items-center">
-        <div class="d-flex justify-content-left ">
-            &nbsp&nbsp&nbsp
-            &nbsp&nbsp&nbsp
+        &nbsp&nbsp&nbsp &nbsp&nbsp&nbsp
+        <div class="pt-2">
+          <div class="d-flex justify-content-left">
+            &nbsp&nbsp&nbsp &nbsp&nbsp&nbsp
             <div class="row d-flex">
-                <div class="row g-0 text-center pt-3 ">
-                    <div id="cajas" class="col-sm-6 col-md-4">10m</div>
-                    <div id="cajas2" class="col-md-5  pt-2">Tiempo llamadas</div>
+              <div class="row g-0 text-center pt-3">
+                <div id="cajas" class="col-sm-6 col-md-4">${this.tableData.length}
                 </div>
-                &nbsp&nbsp&nbsp
-                &nbsp&nbsp&nbsp
-                &nbsp&nbsp&nbsp
-                <div class="row g-0 text-center pt-3 ">
-                    <div id="cajas" class="col-sm-6 col-md-4">10m</div>
-                    <div id="cajas2" class="col-md-5  pt-2">Tiempo llamada actual</div>
-                </div>
-                &nbsp&nbsp&nbsp
-                &nbsp&nbsp&nbsp
-                &nbsp&nbsp&nbsp
-                <div class="row g-0 text-center pt-3 ">
-                    <div id="cajasi2" class="col-sm-6 col-md-7">Campaña 1</div>
-                    <div id="cajas2" class="col-md-5  pt-2">Campañas Activas</div>
-                </div>
+                <div id="cajas2" class="col-md-5 pt-2">Campañas Activas</div>
+              </div>
             </div>
-        </div>
-        <br>
+          </div>
+          <br />
 
-        <div id="cuadro" class="container border border-dark">
-            <div class="d-flex p-3" >
-                <div class=" border border-dark" style="width: 14rem; height: 33.3rem; border-radius: 1rem;">
-                    <div>
-                        <div class="d-flex  flex-shrink-0 p-3" style="width: 222px; background-color: #8ebc99; border-top-left-radius: 1rem; border-top-right-radius: 1rem; border-bottom-left-radius: 1rem; border-bottom-right-radius: 1rem; height: 33.1rem; ">
-                            
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div class="d-flex justify-content-around ">
-                        <div class=" ml-5">
-                            <div class="row g-0 text-center pt-1 ">
-                               
-                            </div>       
-                        </div>
-                        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                        <div class="d-flex justify-content-right ml-5">
-                        <button @click="${this.openNewUserForm}" class="mt-1 text-center" style="width: 5rem; height: 40px; border-radius: 5px; border: #101a11; background-color: #101a11; color: white;">NUEVO</button>
-
-                    </div>
-
-                        </div>
-                    </div>
-                 
-                    <div class="container border border-dark mt-4 ml-4" style="border-radius: 1rem; width: 40rem; height: 26.5rem;">
-
-                    <button @click="${this.activarLista}" class="btn btn-success mr-3">Activar</button>
-                    <button @click="${this.desactivarLista}" class="btn btn-danger">Desactivar</button>
-
-                    
-                    &nbsp&nbsp&nbsp
-                    &nbsp&nbsp&nbsp
-                    <ul class="list-group">
-                    ${this.buscarUsuarios().map((user) => html`
-                      <li class="list-group-item">
-                        <div class="d-flex justify-content-between">
-                          <div>
-                           
-                            <span>Nombre: ${user.nombre}</span><br>
-                            <span>Empresa: ${user.empresa}</span><br>
-                            <span>Equipo: ${user.equipo}</span><br>
-                            <span>Inicia: ${user.inicia}</span><br>
-                            <span>Termina: ${user.termina}</span><br>
-                          </div>
-                          <div>
-                            
-                          </div>
-                        </div>
-                      </li>
-                    `)}
-                  </ul>
-                        </div>
-                    </div>
-
-                    
-                       
-                    </div>
-                </div>
-                </div>
-                
-            </div>
+          <div id="cuadro" class="container border border-dark ml-2">
+            <div class="d-flex p-3">
             
-        </div> 
-    </div>   
-</div>    
+              <div
+                  class="container border border-dark mt-4 ml-4"
+                  style="border-radius: 1rem; width: 15rem; height: 21rem;"
+                >
+                  <!-- Aquí se muestra la sección de detalles -->
+                  <h3>Detalles del usuario seleccionado:</h3>
+                  ${this.campanasencontrado
+        ? html`
+                        <p><strong>Nombre:</strong> ${this.campanasencontrado.nombre}</p>
+                        <p><strong>Equipo:</strong> ${this.campanasencontrado.equipo}</p>
+                        <p><strong>Campaña:</strong> ${this.campanasencontrado.campana}</p>
+                        <p><strong>Estado:</strong> ${this.campanasencontrado.activa ? 'Activa' : 'Inactiva'}</p>
+                      `
+        : html`
+                        <p>Selecciona un usuario para ver sus detalles.</p>
+                      `}
+                </div>
+                  <div
+                    class="d-flex flex-shrink-0 p-3"
+                    style="width: 3px; border-top-left-radius: 1rem; border-top-right-radius: 1rem; height: 5rem; "
+                  >
+                    <hr />
+                    
+              </div>
+              <div>
+                <div class="d-flex justify-content-around">
+                  <div class="ml-5 d-flex justify-content-around">
+                    <div class="row g-0 text-center pt-1">
+                      <button
+                        id="btnFiltrar"
+                        class="col-sm-6 col-md-4 border border-secondary font-weight-bold"
+                        style="font-size: 19px; height: 68px; border-radius: 7px;"
+                      >
+                        Filtrar por campaña</button
+                      >&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                      <button
+                        @click=${this.openModal}
+                        class="mt-1 text-center"
+                        style="width: 70px; height: 40px; border-radius: 5px; border: #343A40; background-color: #343A40; color: white;"
+                      >
+                        Nuevo
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div id="myModal" class="modal">
+                  <div class="modal-content">
+                    <span class="close" @click="${this.closeModal}">&times;</span>
+                    <h2>Nuevo Registro de Compañía</h2>
+                    <input id="nombre" type="text" placeholder="Nombre de la Compañía" />
+                    <input id="equipo" type="text" placeholder="Nombre del Equipo" />
+                    <input id="campana" type="text" placeholder="Nombre de la Campaña" />
+                    <button @click="${this.registrarCompania}">Registrar Compañía</button>
+                  </div>
+                </div>
+
+                <div
+                  class="container border border-dark mt-4 ml-4"
+                  style="border-radius: 1rem; width: 40rem; height: 21rem;"
+                >
+                 <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>Nombre</th>
+                      <th>Equipo</th>
+                      <th>Campaña</th>
+                      <th>Estado</th>
+                      <th>Acciones</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    ${this.tableData.map((user, index) => html`
+                      <tr>
+                        <td>${index + 1}</td>
+                        <td>${user.nombre}</td>
+                        <td>${user.equipo}</td>
+                        <td>${user.campana}</td>
+                        <td>${user.activa ? 'Activa' : 'Inactiva'}</td>
+                        <td style="display: flex; gap: 5px;">
+                          <button @click=${() => this.toggleCampaignStatus(index)} class="btn btn-sm btn-primary custom-button">
+                            ${user.activa ? 'Desactivar' : 'Activar'} 
+                          </button>
+                          <button @click=${() => this.showEquipoDetails(user)} class="btn btn-sm  custom-button">
+                            Ver Detalles
+                          </button>
+                        </td>
+                      </tr>
+                    `)}
+                  </tbody>
+                </table>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     `;
   }
 }
